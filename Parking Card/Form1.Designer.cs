dddce4e99545e34d5f1144bcc5dfbb8e05593e99
vv8702in <vv8702in@T305006-1625270>
2020-01-23 14:30:25 -0600

    Add project files.

---
+++ Parking Card/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Parking_Card
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCreditAdded = new System.Windows.Forms.TextBox();
+            this.txtOneParkNoCard = new System.Windows.Forms.TextBox();
+            this.txtOneParkWithCard = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.txtDaysParking = new System.Windows.Forms.TextBox();
+            this.txtCreditRemaining = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtSavings = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtCreditAdded
+            // 
+            this.txtCreditAdded.Location = new System.Drawing.Point(417, 76);
+            this.txtCreditAdded.Name = "txtCreditAdded";
+            this.txtCreditAdded.Size = new System.Drawing.Size(100, 20);
+            this.txtCreditAdded.TabIndex = 0;
+            this.txtCreditAdded.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtOneParkNoCard
+            // 
+            this.txtOneParkNoCard.Location = new System.Drawing.Point(417, 111);
+            this.txtOneParkNoCard.Name = "txtOneParkNoCard";
+            this.txtOneParkNoCard.Size = new System.Drawing.Size(100, 20);
+            this.txtOneParkNoCard.TabIndex = 1;
+            this.txtOneParkNoCard.Text = "5";
+            this.txtOneParkNoCard.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtOneParkWithCard
+            // 
+            this.txtOneParkWithCard.Location = new System.Drawing.Point(417, 147);
+            this.txtOneParkWithCard.Name = "txtOneParkWithCard";
+            this.txtOneParkWithCard.Size = new System.Drawing.Size(100, 20);
+            this.txtOneParkWithCard.TabIndex = 2;
+            this.txtOneParkWithCard.Text = "2.50";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(442, 186);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // txtDaysParking
+            // 
+            this.txtDaysParking.Location = new System.Drawing.Point(417, 270);
+            this.txtDaysParking.Name = "txtDaysParking";
+            this.txtDaysParking.ReadOnly = true;
+            this.txtDaysParking.Size = new System.Drawing.Size(100, 20);
+            this.txtDaysParking.TabIndex = 4;
+            // 
+            // txtCreditRemaining
+            // 
+            this.txtCreditRemaining.Location = new System.Drawing.Point(417, 306);
+            this.txtCreditRemaining.Name = "txtCreditRemaining";
+            this.txtCreditRemaining.ReadOnly = true;
+            this.txtCreditRemaining.Size = new System.Drawing.Size(100, 20);
+            this.txtCreditRemaining.TabIndex = 5;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(442, 393);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(202, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Credit added to Campus Card";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(202, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Parking cost without Campus Card";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(202, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Parking cost with Campus Card";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(202, 277);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Number of days parking";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(202, 313);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Credit remaining";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(202, 356);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(133, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Savings with Campus Card";
+            // 
+            // txtSavings
+            // 
+            this.txtSavings.Location = new System.Drawing.Point(417, 349);
+            this.txtSavings.Name = "txtSavings";
+            this.txtSavings.ReadOnly = true;
+            this.txtSavings.Size = new System.Drawing.Size(100, 20);
+            this.txtSavings.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtSavings);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtCreditRemaining);
+            this.Controls.Add(this.txtDaysParking);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtOneParkWithCard);
+            this.Controls.Add(this.txtOneParkNoCard);
+            this.Controls.Add(this.txtCreditAdded);
+            this.Name = "Form1";
+            this.Text = "Parking Card Helper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCreditAdded;
+        private System.Windows.Forms.TextBox txtOneParkNoCard;
+        private System.Windows.Forms.TextBox txtOneParkWithCard;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox txtDaysParking;
+        private System.Windows.Forms.TextBox txtCreditRemaining;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtSavings;
+    }
+}
+
